fdd812115915331bc657b1fe909a9ee596a19289
Chris <chris@techlinksolutions.com>
2018-10-02 23:08:28 -0400

    Concat, minify, and write scripts to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function () {
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
+var DEST = 'dist';
+
+gulp.task('scripts', function scripts() {
+  return gulp
+    .src('js/**/*.js')
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        dirname: 'scripts',
+        extname: '.min.js'
+      })
+    )
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('default', function() {
   // place code for your default task here
 });
