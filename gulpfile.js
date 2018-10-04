4a69d92df55e98ee457c5b2184bcd7c96ef2d0bf
Chris <chris@techlinksolutions.com>
2018-10-03 22:19:04 -0400

    Add build task

--- gulpfile.js
+++ gulpfile.js
@@ -11,37 +11,60 @@ var del = require('del');
 
 var DEST = 'dist';
 
-gulp.task('scripts', function scripts() {
-  return gulp
-    .src('js/**/*.js')
-    .pipe(smaps.init())
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(rename({ dirname: 'scripts', extname: '.min.js' }))
-    .pipe(smaps.write('./'))
-    .pipe(gulp.dest(DEST));
+gulp.task('clean:scripts', function() {
+  return del(DEST + '/scripts');
 });
 
-gulp.task('styles', function styles() {
-  return gulp
-    .src('sass/**/*.scss')
-    .pipe(smaps.init())
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(rename({ dirname: 'styles', basename: 'all', extname: '.min.css' }))
-    .pipe(smaps.write('./'))
-    .pipe(gulp.dest(DEST));
+gulp.task(
+  'scripts',
+  gulp.series('clean:scripts', function scripts() {
+    return gulp
+      .src('js/**/*.js')
+      .pipe(smaps.init())
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+      .pipe(rename({ dirname: 'scripts', extname: '.min.js' }))
+      .pipe(smaps.write('./'))
+      .pipe(gulp.dest(DEST));
+  })
+);
+
+gulp.task('clean:styles', function() {
+  return del(DEST + '/styles');
 });
 
-gulp.task('images', function images() {
-  return gulp
-    .src('images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest(DEST + '/content'));
+gulp.task(
+  'styles',
+  gulp.series('clean:styles', function styles() {
+    return gulp
+      .src('sass/**/*.scss')
+      .pipe(smaps.init())
+      .pipe(sass({ outputStyle: 'compressed' }))
+      .pipe(rename({ dirname: 'styles', basename: 'all', extname: '.min.css' }))
+      .pipe(smaps.write('./'))
+      .pipe(gulp.dest(DEST));
+  })
+);
+
+gulp.task('clean:images', function() {
+  return del(DEST + '/content');
 });
+
+gulp.task(
+  'images',
+  gulp.series('clean:images', function images() {
+    return gulp
+      .src('images/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest(DEST + '/content'));
+  })
+);
 
 gulp.task('clean', function clean() {
   return del(DEST);
 });
+
+gulp.task('build', gulp.parallel('scripts', 'styles', 'images'));
 
 gulp.task('default', function() {
   // place code for your default task here
