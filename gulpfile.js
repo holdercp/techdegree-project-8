34abf790029cf0ca073942ca33ef70a7bf1c92ad
Chris <chris@techlinksolutions.com>
2018-10-03 21:28:43 -0400

    Add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,28 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var smaps = require('gulp-sourcemaps');
 
 var DEST = 'dist';
 
 gulp.task('scripts', function scripts() {
   return gulp
     .src('js/**/*.js')
+    .pipe(smaps.init())
     .pipe(concat('all.js'))
     .pipe(uglify())
     .pipe(rename({ dirname: 'scripts', extname: '.min.js' }))
+    .pipe(smaps.write('./'))
     .pipe(gulp.dest(DEST));
 });
 
 gulp.task('styles', function styles() {
   return gulp
     .src('sass/**/*.scss')
+    .pipe(smaps.init())
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(rename({ dirname: 'styles', basename: 'all', extname: '.min.css' }))
+    .pipe(smaps.write('./'))
     .pipe(gulp.dest(DEST));
 });
 
